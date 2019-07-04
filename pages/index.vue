7f50045936066732c019baad19d84101b861caf3
Dmitriy Nosovskiy <disscovery.993@gmail.com>
2019-07-04 22:12:12 +0300

    remove base html

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,19 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        medalnica
-      </h1>
-      <h2 class="subtitle">
-        medalnica app
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    aaaaa
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
+  components: {},
+  data() {
+    return {}
   }
 }
 </script>
 
 <style lang="sass">
-.container
-  margin: 0 auto
-  min-height: 100vh
-  display: flex
-  justify-content: center
-  align-items: center
-  text-align: center
-
-.title
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif
-  display: block
-  font-weight: 300
-  font-size: 100px
-  color: $blue
-  letter-spacing: 1px
-
-
-.subtitle
-  font-weight: 300
-  font-size: 42px
-  color: #526488
-  word-spacing: 5px
-  padding-bottom: 15px
-
-
-.links
-  padding-top: 15px
 
 </style>
